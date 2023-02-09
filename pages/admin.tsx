6a9884816a35ba0e383f1d917b3958801c27fb82
Zizwar <zizwar@gmail.com>
2023-02-09 03:58:55 +0000

    add multer upload file

---
+++ pages/admin.tsx
@@ -0,0 +1,128 @@
+
+import Layout from "../layouts/Main";
+import { UiFileInputButton }from '../components/files-upload';
+import axios from "axios";
+const CheckoutPage = () => {
+
+  const onChange = async (formData) => {
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' },
+      onUploadProgress: (event) => {
+        console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+      },
+    };
+
+    const response = await axios.post('/api/admin/product', formData, config);
+
+    console.log('response', response.data);
+  };
+
+  return (
+    <Layout>
+      <section className="cart">
+        <div className="container">
+          <div className="cart__intro">
+            <h3 className="cart__title">Admin</h3>
+          </div>
+
+          <UiFileInputButton
+      label="Upload Single File"
+      uploadFileName="theFiles"
+      onChange={onChange}
+    />
+
+          <div className="checkout-content">
+            <div className="checkout__col-6">
+              <div className="checkout__btns">
+                <button className="btn btn--rounded btn--yellow">Log in</button>
+                <button className="btn btn--rounded btn--border">
+                  Sign up
+                </button>
+              </div>
+
+              <div className="block">
+                <h3 className="block__title">Shipping information</h3>
+                <form className="form">
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Email"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Address"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="First name"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="City"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Last name"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Postal code / ZIP"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Phone number"
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <div className="select-wrapper select-form">
+                        <select>
+                          <option>Country</option>
+                          <option value="Argentina">Argentina</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default CheckoutPage;
