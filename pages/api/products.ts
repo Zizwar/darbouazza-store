5c394bb243d983c651ccd9db01e247d610fa1ef1
Zizwar <zizwar@gmail.com>
2023-02-27 02:05:12 +0000

    inspect variable json  from post

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,17 +1,29 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 import useBlogger from "../../wino/";
 // fake data
 
-
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
- // console.log(req);
- const usb = new useBlogger(); 
- const data = await usb.load();
- res.json(data); 
-   /*
+  // console.log(req);
+
+  const variables: any = {
+    price: "number",
+    discount: "number",
+    quantityAvailable: "number",
+    currentPrice: "number",
+    sizes: "array",
+    colors: "array",
+  };
+  //
+
+  //
+  const usb = new useBlogger();
+
+  const data = await usb.load(variables);
+  res.json(data);
+  /*
   const data:any = await useBlogger([]);
   console.log("==");
  
   res.json(data);
   */
-}
+};
